d615e09f560e3be6f5bc090cf2c10cdbb8e20ade
Madhumita <madhu@gluu.org>
2023-02-13 15:32:54 +0530

    fix: #3771

--- jans-fido2/server/src/main/java/io/jans/fido2/service/operation/AssertionService.java
+++ jans-fido2/server/src/main/java/io/jans/fido2/service/operation/AssertionService.java
@@ -172,12 +172,6 @@ public class AssertionService {
 				ObjectNode extensions = (ObjectNode) optionsResponseNode.get("extensions");
 				extensions.put("appid", fidoApplicationId);
 			} 
-			// TODO: this doesnt make any sense - test and remove
-			/*else {
-				ObjectNode extensions = dataMapperService.createObjectNode();
-				extensions.put("appid", fidoApplicationId);
-				optionsResponseNode.set("extensions", extensions);
-			}*/
 		}
 
 		// optionsResponseNode.put("status", "ok");
